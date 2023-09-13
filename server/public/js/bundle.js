83991a77bb4f50a232c58c93055592ba3b1eff67
WesleyIcaro <wesleyicaro4@gmail.com>
2023-09-13 00:26:48 -0300

    Change listen for 8081.

--- server/public/js/bundle.js
+++ server/public/js/bundle.js
@@ -66,7 +66,7 @@ function _initAddress() {
                                 e.target.value = e.target.value.replace(/(-\d{3})\d+?$/, '$1');
                                 _context.prev = 4;
                                 _context.next = 7;
-                                return fetch("http://localhost:8080/cadastrarEndereco?termo=".concat(cepSemPonto));
+                                return fetch("http://localhost:8081/cadastrarEndereco?termo=".concat(cepSemPonto));
                               case 7:
                                 _response = _context.sent;
                                 if (!_response.ok) {
@@ -1115,7 +1115,7 @@ function _createFormSchoolData() {
                             case 0:
                               _context.prev = 0;
                               _context.next = 3;
-                              return fetch("http://localhost:8080/cadastrarCurso?termo=".concat(codeFinal));
+                              return fetch("http://localhost:8081/cadastrarCurso?termo=".concat(codeFinal));
                             case 3:
                               _response = _context.sent;
                               if (!_response.ok) {
@@ -1165,7 +1165,7 @@ function _createFormSchoolData() {
                                       element = e.target;
                                       _context2.prev = 1;
                                       _context2.next = 4;
-                                      return fetch("http://localhost:8080/cadastrarEscola?termo=".concat(element.value));
+                                      return fetch("http://localhost:8081/cadastrarEscola?termo=".concat(element.value));
                                     case 4:
                                       _response2 = _context2.sent;
                                       if (!_response2.ok) {
@@ -1757,7 +1757,7 @@ var isSchool = /*#__PURE__*/function () {
           likeSchool = {};
           _context3.prev = 1;
           _context3.next = 4;
-          return fetch("http://localhost:8080/cadastrarEscola?termo=".concat(school));
+          return fetch("http://localhost:8081/cadastrarEscola?termo=".concat(school));
         case 4:
           response = _context3.sent;
           if (!response.ok) {
@@ -1843,7 +1843,7 @@ var isCourse = /*#__PURE__*/function () {
           likeCourse = {};
           _context4.prev = 1;
           _context4.next = 4;
-          return fetch("http://localhost:8080/cadastrarCurso?termo=".concat(codeCourse));
+          return fetch("http://localhost:8081/cadastrarCurso?termo=".concat(codeCourse));
         case 4:
           response = _context4.sent;
           if (!response.ok) {
@@ -2496,7 +2496,7 @@ function _sendData() {
           data = _context2.sent;
           _context2.prev = 3;
           _context2.next = 6;
-          return fetch('http://localhost:8080/cadastrar', {
+          return fetch('http://localhost:8081/cadastrar', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'

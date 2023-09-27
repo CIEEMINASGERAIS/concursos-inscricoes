15e74f096b4c4bbd245d2b929de370669a7c47df
WesleyIcaro <wesleyicaro4@gmail.com>
2023-09-27 12:17:20 -0300

    Change for listening of all machines.

--- app.js
+++ app.js
@@ -255,6 +255,10 @@ app.post('/cadastrar', async (req, res) => {
 })
 
 // Iniciar o servidor na porta 8080, criar a função utilizando modelo Arrow function para retornar a mensagem de sucesso
-app.listen(8080, () => {
-  console.log('Servidor iniciado na porta 8080: http://localhost:8080')
+// app.listen(8080, () => {
+//   console.log('Servidor iniciado na porta 8080: http://localhost:8080')
+// })
+
+app.listen(8080, '0.0.0.0', () => {
+  console.log('Servidor iniciado na porta 8080: http://0.0.0.0:8080')
 })

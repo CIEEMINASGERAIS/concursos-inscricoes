7d8877fa7ef02282a9332c86f37000fd31f6a591
WesleyIcaro <wesleyicaro4@gmail.com>
2023-10-20 10:59:13 -0300

    Work in email.

--- src/controllers/enviarEmail.js
+++ src/controllers/enviarEmail.js
@@ -21,59 +21,61 @@ async function emailASerEnviado(to, user) {
         to: `${to}`,
         subject: 'Senha para acesso do Portal Estudante',
         html: ` 
-        <section>
-            <img alt="Logo do CIEEMG" src="images/CIEE-2023-atualizado.png" id="logo-email" />
+        <div id="box-email">
+            <img alt="Logo do CIEEMG" src="https://cieemg.org.br/files/2023-09/logooficialsimplificada2023.png?58d4bae036"
+                id="logo-email" />
             <div id="line1-message"></div>
             <p class="body">Prezado(a) <b>Wesley Icaro Lima dos Santos</b>,
             </p>
             <p class="body">
-        
+
                 Esta é a sua senha, pedimos que altere sua senha no primeiro acesso ao sistema.</p>
-        
+
             <p class="body">A senha é: <b>CSv0y1</b>.</p>
             <div id="line2-message"></div>
             <p class="foot">CIEEMG - Centro de Integração Empresa Escola de Minas Gerais</p>
             <p class="foot">Portal CIEE/MG (www.cieemg.org.br)</p>
-        </section>
+        </div>
         <style>
             * {
+                box-sizing: border-box;
                 margin: 0;
                 padding: 0;
-                outline: none;
+                outline: 0;
+                font-family: 'Inter', sans-serif;
             }
-        
-            section {
-                background-color: white;
+
+            #box-email {
                 height: 100%;
                 border: 1px solid rgb(190, 190, 190);
                 padding: 1rem;
-                width: 25rem;
+                max-width: 30rem;
                 height: 18rem;
-                background-color: rgb(161, 152, 204);
+                background-color: white;
                 margin: 0rem auto;
             }
-        
+
             .body {
                 margin: 1rem 0rem;
             }
-        
-        
+
+
             #logo-email {
-                height: 4rem;
+                height: 3rem;
             }
-        
+
             #line1-message {
                 height: 1px;
                 background-color: rgb(216, 216, 216);
-                margin: 0rem 0rem 1rem 0rem;
+                margin: 1rem 0rem 1rem 0rem;
             }
-        
+
             #line2-message {
                 height: 1px;
                 background-color: rgb(216, 216, 216);
                 margin: 1rem 0rem 0.5rem 0rem;
             }
-        
+
             .foot {
                 font-size: 0.7em;
                 font-weight: 800;

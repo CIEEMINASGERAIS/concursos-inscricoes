4bf3392a0020f92f22c5aa81b2406ae56199fc21
WesleyIcaro <wesleyicaro4@gmail.com>
2023-10-24 08:55:41 -0300

    Update in menu and inputs warnings.

--- src/controllers/enviarEmail.js
+++ src/controllers/enviarEmail.js
@@ -1,8 +1,9 @@
 const nodemailer = require("nodemailer")
 
 const tranposter = nodemailer.createTransport({
+    name: 'nao_responda@cieeminas.org.br',
     host: '192.168.0.11',
-    port: 465,
+    port: 587,
     secure: true,
     auth: {
         user: 'nao_responda@cieeminas.org.br',
@@ -21,68 +22,70 @@ async function emailASerEnviado(to, user, pass) {
         to: `${to}`,
         subject: 'Senha para acesso do Portal Estudante',
         html: ` 
-        <div id="box-email">
-            <img alt="Logo do CIEEMG" src="https://cieemg.org.br/files/2023-09/logooficialsimplificada2023.png?58d4bae036"
-                id="logo-email" />
-            <div id="line1-message"></div>
-            <p class="body">Prezado(a) <b>${user}</b>,
-            </p>
-            <p class="body">
+        <html>
+            <div id="box-email">
+                <img alt="Logo do CIEEMG" src="https://cieemg.org.br/files/2023-09/logooficialsimplificada2023.png?58d4bae036"
+                    id="logo-email" />
+                <div id="line1-message"></div>
+                <p class="body">Prezado(a) <b>${user}</b>,
+                </p>
+                <p class="body">
 
-                Esta é a sua senha, pedimos que altere sua senha no primeiro acesso ao sistema.</p>
+                    Esta é a sua senha, pedimos que altere sua senha no primeiro acesso ao sistema.</p>
 
-            <p class="body">A senha é: <b>${pass}</b>.</p>
-            <div id="line2-message"></div>
-            <p class="foot">CIEEMG - Centro de Integração Empresa Escola de Minas Gerais</p>
-            <p class="foot">Portal CIEE/MG (www.cieemg.org.br)</p>
-        </div>
-        <style>
-            * {
-                box-sizing: border-box;
-                margin: 0;
-                padding: 0;
-                outline: 0;
-                font-family: 'Inter', sans-serif;
-            }
+                <p class="body">A senha é: <b>${pass}</b>.</p>
+                <div id="line2-message"></div>
+                <p class="foot">CIEEMG - Centro de Integração Empresa Escola de Minas Gerais</p>
+                <p class="foot">Portal CIEE/MG (www.cieemg.org.br)</p>
+            </div>
+            <style>
+                * {
+                    box-sizing: border-box;
+                    margin: 0;
+                    padding: 0;
+                    outline: 0;
+                    font-family: 'Inter', sans-serif;
+                }
 
-            #box-email {
-                height: 100%;
-                border: 1px solid rgb(190, 190, 190);
-                padding: 1rem;
-                max-width: 30rem;
-                height: 18rem;
-                background-color: white;
-                margin: 0rem auto;
-            }
+                #box-email {
+                    height: 100%;
+                    border: 1px solid rgb(190, 190, 190);
+                    padding: 1rem;
+                    max-width: 30rem;
+                    height: 18rem;
+                    background-color: white;
+                    margin: 0rem auto;
+                }
 
-            .body {
-                margin: 1rem 0rem;
-            }
+                .body {
+                    margin: 1rem 0rem;
+                }
 
 
-            #logo-email {
-                height: 3rem;
-            }
+                #logo-email {
+                    height: 3rem;
+                }
 
-            #line1-message {
-                height: 1px;
-                background-color: rgb(216, 216, 216);
-                margin: 1rem 0rem 1rem 0rem;
-            }
+                #line1-message {
+                    height: 1px;
+                    background-color: rgb(216, 216, 216);
+                    margin: 1rem 0rem 1rem 0rem;
+                }
 
-            #line2-message {
-                height: 1px;
-                background-color: rgb(216, 216, 216);
-                margin: 1rem 0rem 0.5rem 0rem;
-            }
+                #line2-message {
+                    height: 1px;
+                    background-color: rgb(216, 216, 216);
+                    margin: 1rem 0rem 0.5rem 0rem;
+                }
 
-            .foot {
-                font-size: 0.7em;
-                font-weight: 800;
-                text-align: center;
-                margin: 0.5rem;
-            }
-        </style>
+                .foot {
+                    font-size: 0.7em;
+                    font-weight: 800;
+                    text-align: center;
+                    margin: 0.5rem;
+                }
+            </style>
+        </html>
          `,
     })
 

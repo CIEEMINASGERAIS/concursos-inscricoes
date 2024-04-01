4584c0f074aef5670b9fbc1a558e456cc943db25
Wesley Icaro Lima dos Santos <wesleyicaro4@gmail.com>
2024-04-01 15:00:16 -0300

    Update in programas em curso.

--- frontend/pages/programasEmCurso.js
+++ frontend/pages/programasEmCurso.js
@@ -1,3 +1,5 @@
+const selecionarNomes = require("../utils/util")
+
 async function programasEmCurso() {
     const response = await fetch("programa-curso")
 
@@ -6,6 +8,19 @@ async function programasEmCurso() {
     const screenProgramasCurso = document.querySelector(".screen-programa-curso")
 
     screenProgramasCurso.innerHTML = htmlContent
+
+    const programasEmCurso = document.getElementsByTagName("programas-curso")
+
+    const selectProgramaCurso = document.createElement("select")
+
+    const divProgramas = document.querySelector(".form-group-programas")
+
+    divProgramas.insertAdjacentElement("afterend", selectProgramaCurso)
+
+    if (selecionarNomes(programasEmCurso) === "Sim") {
+        CreateInputLabel
+    }
+
 }
 
 module.exports = { programasEmCurso }

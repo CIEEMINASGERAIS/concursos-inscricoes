7b1c7110e963bf02063e89d08a458f5e4a2d021f
Wesley Icaro Lima dos Santos <wesleyicaro4@gmail.com>
2024-04-19 14:01:53 -0300

    Update in address.

--- frontend/pages/app.js
+++ frontend/pages/app.js
@@ -148,7 +148,7 @@ async function sendData() {
   const data = await takeData();  
 
   try {
-    const response = await fetch("http://localhost:8080/cadastrar", {
+    const response = await fetch("https://appcadastro.cieemg.org.br/cadastrar", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",

38fb9b2cff28158d63d7183ea932ebf4db47f6d5
VITAO23 <victorhugonunesoliveira2002@gmail.com>
2026-06-11 10:02:46 -0300

    https in address

--- frontend/pages/address.js
+++ frontend/pages/address.js
@@ -147,7 +147,7 @@ async function initAddress() {
         if (e.target.value.length === 9) {
           try {
             const response = await fetch(
-              `http://appcadastro.cieemg.org.br/cadastrarEndereco?termo=${cepSemPonto}`
+              `https://appcadastro.cieemg.org.br/cadastrarEndereco?termo=${cepSemPonto}`
             )
             if (response.ok) {
               const opcoes = await response.json()

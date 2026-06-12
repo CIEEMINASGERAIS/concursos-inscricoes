ae72c0c6cc063f56a56a2d334460b056d3e2d65b
VITAO23 <victorhugonunesoliveira2002@gmail.com>
2026-06-12 16:57:10 -0300

    new helper for url relative

--- app.js
+++ app.js
@@ -54,8 +54,7 @@ app.set("view engine", "ejs");
 app.use(express.json({ limit: "25mb" }));
 
 const whiteList = [
-  "https://appcadastro.cieemg.org.br",
-  "http://localhost:8080",
+  "https://appcadastro.cieemg.org.br"
 ];
 
 const corsOptions = {
--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,9 +1,5 @@
-// function cadastro(route, id) {
-//     return `https://appcadastro.cieemg.org.br/${route ? route : ''}${id ? `/${id}` : ''}`
-// };
-
 function cadastro(route, id) {
-    return `http://localhost:8080/${route ? route : ''}${id ? `/${id}` : ''}`
+    return `/${route ? route : ''}${id ? `/${id}` : ''}`
 };
 
 
